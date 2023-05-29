26d70abd3967e4698a3f2b922501816f31f83454
isurusachinda85 <isurusachintha8@gmail.com>
2023-05-30 04:36:57 +0530

    update

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ public class LoginFormController {
         stage.setScene(new Scene(FXMLLoader.load(LoginFormController.class.getResource("../view/Client01.fxml"))));
         stage.close();
         stage.setTitle("Chat Application");
-//        stage.getIcons().add(new Image("lk/ijse/chatApp/asserts/doulChat.png"));
+//        stage.getIcons().add(new Image("assest/camera.png"));
         stage.centerOnScreen();
         stage.setResizable(false);
         stage.show();
