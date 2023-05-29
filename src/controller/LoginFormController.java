9d0d97c4e19bf8a52c0ceed927a6ac9e45eed9e8
isurusachinda85 <isurusachintha8@gmail.com>
2023-05-30 01:05:45 +0530

    update

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LoginFormController.class.getResource("../view/Client01.fxml"))));
+        stage.close();
+        stage.setTitle("Chat Application");
+//        stage.getIcons().add(new Image("lk/ijse/chatApp/asserts/doulChat.png"));
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
+    }
 }
